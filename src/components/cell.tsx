8e41129b621ba76ed76147f3795adf3be67bfb1e
Hans Hofner <hans@hhofner.com>
2021-09-23 16:43:35 +0900

    Better working idea

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -14,9 +14,8 @@ const Cell = ({num, lastPosition, onInFrame}: {
     onInFrame: (pos: number | null) => void; // When the child is in Frame, it sends its dot position to the parent so it can distribute it (later)
 }) => {
     const containerRef = useRef<HTMLDivElement>(null);
-    const dotRef = useRef<HTMLDivElement>(null);
+    const dotRef = useRef<HTMLImageElement>(null);
     const [isInFrame, setIsInFrame] = useState<boolean | null>(false);
-    const [lastDotPosition, setLastDotPosition] = useState<number | null>(null);
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -26,7 +25,8 @@ const Cell = ({num, lastPosition, onInFrame}: {
                 containerRef.current.getBoundingClientRect().bottom > 300
             ) {
                 setIsInFrame(true);
-                onInFrame(dotRef.current && dotRef.current.getBoundingClientRect().y); // Then update it here (send to parent)
+                const dot = dotRef.current && dotRef.current.getBoundingClientRect().y
+                onInFrame(dot); // Then update it here (send to parent)
             } else {setIsInFrame(false)}
 
         //    Why does the isInFrame variable not update here?
@@ -40,30 +40,29 @@ const Cell = ({num, lastPosition, onInFrame}: {
             console.log(`${num} is not inFrame`)
             // Has gone out of frame and it was before in frame, so set the dot position as latest
             onInFrame(dotRef.current && dotRef.current.getBoundingClientRect().y);
-        } else {
-            console.log(`${num} is inFrame`)
-            // Gets into frame meaning it needs to translateY back, but it needs to do it after
-            // the parent updates the last dot position.
         }
     }, [isInFrame]);
 
     return (
-        <div className="container" style={{height: "250px"}} ref={containerRef}>
+        <div className="container" style={{height: `250px`}} ref={containerRef}>
             <div className={"sidebar"} style={{display: "flex", flexDirection: "column"}}>
                 <p>Section {num}</p>
                 <div style={{
                     fontSize: "5rem",
                     color: "turquoise",
-                    visibility: isInFrame ? "visible" : "hidden",
-                    transition: "transform 0.4s ease-out",
-                    transform: isInFrame ?
-                        "translateY(0)" : ((lastPosition && dotRef.current) ?
-                            (lastPosition > dotRef.current.getBoundingClientRect().y ?
-                                `translateY(${lastPosition - dotRef.current.getBoundingClientRect().y}px)` :
-                                `translateY(-${dotRef.current.getBoundingClientRect().y - lastPosition}px)`) : "translateY(-10px)")
+                    display: "inline-block",
+                    margin: "auto",
                 }}
                      ref={dotRef}
-                >•</div>
+                ><img ref={dotRef} src={"https://upload.wikimedia.org/wikipedia/commons/c/c0/Location_dot_black.svg"} width={30} height={30}
+                      style={{visibility: isInFrame ? "visible" : "hidden",
+                          transition: "transform 1s ease-out",
+                          transform: isInFrame ?
+                              "translateY(0)" : ((lastPosition && dotRef.current) ?
+                                  (lastPosition > dotRef.current.getBoundingClientRect().y ?
+                                      `translateY(${lastPosition - dotRef.current.getBoundingClientRect().y}px)` :
+                                      `translateY(-${dotRef.current.getBoundingClientRect().y - lastPosition}px)`) : "translateY(0px)")}}
+                /></div>
 
             </div>
             <div className="main">
